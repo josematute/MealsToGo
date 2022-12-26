23fd3307dee36d9d9e627792b25061b0d38de1b1
Jose E Matute <60519538+josematute@users.noreply.github.com>
2022-12-25 21:52:02 -0700

    Adding constants, fonts and renaming some files

--- App.js
+++ App.js
@@ -1,11 +1,27 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar"
+import { ThemeProvider } from "styled-components"
 import RestaurantsScreen from "./src/features/restaurants/screens/RestaurantsScreen"
+import { theme } from "./src/infrastructure/theme"
+
+import { useFonts as useOswald, Oswald_400Regular } from "@expo-google-fonts/oswald"
+import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato"
 
 export default function App() {
+	const [oswaldLoaded] = useOswald({
+		Oswald_400Regular
+	})
+	const [latoLoaded] = useLato({
+		Lato_400Regular
+	})
+
+	if (!oswaldLoaded || !latoLoaded) return null
+
 	return (
 		<>
-			<RestaurantsScreen />
-			<ExpoStatusBar style="auto"></ExpoStatusBar>
+			<ThemeProvider theme={theme}>
+				<RestaurantsScreen />
+				<ExpoStatusBar style="auto"></ExpoStatusBar>
+			</ThemeProvider>
 		</>
 	)
 }
--- src/features/restaurants/screens/RestaurantsScreen.js
+++ src/features/restaurants/screens/RestaurantsScreen.js
@@ -2,27 +2,37 @@ import React from "react"
 import { SafeAreaView, StatusBar, StyleSheet, Text, View } from "react-native"
 import { Searchbar } from "react-native-paper"
 import { SafeAreaProvider } from "react-native-safe-area-context"
-import RestaurantInfo from "../components/RestaurantInfo"
+import styled from "styled-components"
+import RestaurantInfoCard from "../components/RestaurantInfoCard"
+
+const SafeArea = styled(SafeAreaView)`
+	flex: 1;
+	${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`};}
+`
+
+const SearchContainer = styled(View)`
+	padding: ${(props) => props.theme.space[3]};
+`
+
+const RestaurantListContainer = styled(View)`
+	flex: 1;
+	padding: ${(props) => props.theme.space[3]};
+	background-color: blue;
+`
 
 function RestaurantsScreen() {
 	return (
 		<SafeAreaProvider>
-			<SafeAreaView style={styles.container}>
-				<View style={styles.search}>
+			<SafeArea>
+				<SearchContainer>
 					<Searchbar />
-				</View>
-				<View style={styles.list}>
-					<RestaurantInfo />
-				</View>
-			</SafeAreaView>
+				</SearchContainer>
+				<RestaurantListContainer>
+					<RestaurantInfoCard />
+				</RestaurantListContainer>
+			</SafeArea>
 		</SafeAreaProvider>
 	)
 }
-
-const styles = StyleSheet.create({
-	container: { flex: 1, marginTop: StatusBar.currentHeight },
-	search: { padding: 16 },
-	list: { flex: 1, padding: 16, backgroundColor: "blue" }
-})
 
 export default RestaurantsScreen
