a55045496c23898db69b5371664f49cb49ff6462
Jose E Matute <60519538+josematute@users.noreply.github.com>
2022-12-27 18:50:45 -0700

    Structuring restaurant nav and adding maps package

---
+++ src/infrastructure/navigation/appNavigator.js
@@ -0,0 +1,47 @@
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
+import { NavigationContainer } from "@react-navigation/native"
+import React from "react"
+import { Ionicons } from "@expo/vector-icons"
+import { SafeArea as SafeAreaComp } from "../../components/utility/SafeAreaComponent"
+import { Text } from "react-native"
+import { RestaurantsNavigator } from "./restaurantsNavigator"
+import { MapScreen } from "../../features/map/screens/MapScreen"
+
+const Tab = createBottomTabNavigator()
+
+const TAB_ICON = {
+	Restaurants: "md-restaurant",
+	Map: "md-map",
+	Settings: "md-settings"
+}
+
+const createScreenOptions = ({ route }) => {
+	const iconName = TAB_ICON[route.name]
+	return {
+		tabBarIcon: ({ size, color }) => <Ionicons name={iconName} size={size} color={color} />,
+		tabBarActiveTintColor: "tomato",
+		tabBarInactiveTintColor: "gray",
+		tabBarStyle: [
+			{
+				"display": "flex"
+			},
+			null
+		]
+	}
+}
+
+const Settings = () => (
+	<SafeAreaComp>
+		<Text>Settings</Text>
+	</SafeAreaComp>
+)
+
+export const AppNavigator = () => (
+	<NavigationContainer>
+		<Tab.Navigator screenOptions={createScreenOptions}>
+			<Tab.Screen name="Restaurants" component={RestaurantsNavigator} />
+			<Tab.Screen name="Map" component={MapScreen} />
+			<Tab.Screen name="Settings" component={Settings} />
+		</Tab.Navigator>
+	</NavigationContainer>
+)
